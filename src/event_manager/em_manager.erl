c2ca53e7177a0dc7938ebc56041a2249d277db41
Brant Young <anatman.xx@gmail.com>
2013-02-17 17:16:29 +0800

    add event-manager subsystem skeleton

---
+++ src/event_manager/em_manager.erl
@@ -0,0 +1,55 @@
+-module(em_manager).
+
+-behaviour(gen_event).
+
+%% API
+-export([start_link/0,
+         add_handler/2]).
+
+%% gen_event callbacks
+-export([init/1,
+         handle_event/2,
+         handle_call/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+
+-record(state, {}).
+
+
+%%%===================================================================
+%%% gen_event callbacks
+%%%===================================================================
+start_link() ->
+    gen_event:start_link({local, ?MODULE}).
+
+add_handler(Handler, Args) ->
+    gen_event:add_handler(?MODULE, Handler, Args).
+
+
+%%%===================================================================
+%%% gen_event callbacks
+%%%===================================================================
+init([]) ->
+    {ok, #state{}}.
+
+handle_event(_Event, State) ->
+    {ok, State}.
+
+handle_call(_Request, State) ->
+    Reply = ok,
+    {ok, Reply, State}.
+
+handle_info(_Info, State) ->
+    {ok, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
