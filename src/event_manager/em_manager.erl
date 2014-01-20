2bb8abfac4c10216e59a79777cdf1cff4f488487
Brant Young <anatman.xx@gmail.com>
2014-01-20 17:20:21 +0800

    event-manager:     add some interface that controls event manager in event_manager     fix bugs of em_manager     remove useless module em_app and em_sup     remove useless comments

--- src/event_manager/em_manager.erl
+++ src/event_manager/em_manager.erl
@@ -1,53 +1,28 @@
 -module(em_manager).
 
--behaviour(gen_event).
-
 %% API
 -export([start_link/0,
-         add_handler/2]).
-
-%% gen_event callbacks
--export([init/1,
-         handle_event/2,
-         handle_call/2,
-         handle_info/2,
-         terminate/2,
-         code_change/3]).
+         stop/0,
+         add_handler/2,
+         delete_handler/2]).
 
 -record(state, {}).
 
 
 %%%===================================================================
-%%% gen_event callbacks
+%%% API functions
 %%%===================================================================
 start_link() ->
     gen_event:start_link({local, ?MODULE}).
 
+stop() ->
+    gen_event:stop(?MODULE).
+
 add_handler(Handler, Args) ->
     gen_event:add_handler(?MODULE, Handler, Args).
 
-
-%%%===================================================================
-%%% gen_event callbacks
-%%%===================================================================
-init([]) ->
-    {ok, #state{}}.
-
-handle_event(_Event, State) ->
-    {ok, State}.
-
-handle_call(_Request, State) ->
-    Reply = ok,
-    {ok, Reply, State}.
-
-handle_info(_Info, State) ->
-    {ok, State}.
-
-terminate(_Reason, _State) ->
-    ok.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+delete_handler(Handler, Args) ->
+    gen_event:delete_handler(?MODULE, Handler, Args).
 
 
 %%%===================================================================
